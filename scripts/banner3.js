57e726bef24fe9391c1ba3f87e8b365a8ed3af40
Cleber S. Lopes Jr <47905314+cleberslopesjr@users.noreply.github.com>
2023-04-23 18:21:39 -0300

    Commit

---
+++ scripts/banner3.js
@@ -0,0 +1,2 @@
+var banner3=document.querySelector('#divBanner3');
+banner3.innerHTML='<a href="https://bahiasemfome.ba.gov.br/" target="_blank" rel="external"><img src="imagens/banners/BNI_AMARELO_728x90_SAIBA-COMO-PARTICIPAR.png" alt="Banner de propaganda da campanha: A Bahia Contra a Fome do Governo do Estado." title="Banner de propaganda da campanha: A Bahia Contra a Fome do Governo do Estado."></a>';
